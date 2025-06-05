3bd8c8557aed50e304b50c3be288afe178d21f1a
Michael Suárez <71056455+MichaelSuarez0@users.noreply.github.com>
2025-06-04 23:57:37 -0500

    Update c_cleaner.py

--- src_playwright/c_cleaner.py
+++ src_playwright/c_cleaner.py
@@ -27,107 +27,128 @@ Usage:
 # =====================
 import os
 import pandas as pd
-import a_config
+import logging
+from ubigeos_peru import Ubigeo as ubg
+from .a_config import PATH_DATA_PRO, PATH_DATA_RAW
+
+logging.getLogger('consulta_amigable')
 
 # =====================
 # Importación de data
 # =====================
 
-# TODO: Convertir a clase tmb
+# TODO: Convertir a clase tmb, un argumento para leer un excel y otro un df (integrar con consultaamigable)
 
-def read_files(ruta_archivo, hoja=0):
-    """
-    Lee un archivo Excel (.xlsx, .xls) o CSV (.csv) y devuelve un DataFrame.
-    """
-    try:
-        if ruta_archivo.endswith((".xlsx", ".xls")):
-            return pd.read_excel(ruta_archivo, sheet_name=hoja)
-        elif ruta_archivo.endswith(".csv"):
-            print(f"{ruta_archivo} leido.")
-            return pd.read_csv(ruta_archivo)
+class Cleaner:
+    encabezados = [
+        ("Departamento", ["UBI_DPTO", "Departamento"], ":"),
+        ("Provincia", ["UBI_PROV", "Provincia"], ":"),
+        ("Municipalidad", ["UBI_DIST", "COD_SIAF", "Municipalidad"], "-|:"),
+        ("Sector", ["COD_SEC", "Sector"], ":"),
+        ("Pliego", ["COD_PLI", "Pliego"], ":"),
+        ("Unidad Ejecutora", ["UE", "SEC_EJEC", "Unidad Ejecutora"], "-|:"),
+    ]
+
+    def __init__(self, input: pd.DataFrame | str):
+        self.input = input
+        self.df = None
+        self._obtain_df()
+
+    def _obtain_df(self):
+        if isinstance(self.input, pd.DataFrame):
+            self.df = self.input
+        elif isinstance(self.input, str):
+            self.df = self.read_files()
         else:
-            raise ValueError("Formato no soportado. Usa .xlsx, .xls o .csv.")
-    except Exception as e:
-        print(f"Error al leer el archivo '{ruta_archivo}': {e}")
-        return None
+            raise TypeError("Not a valid input type, should be either DataFrame or path (str)")
+        
+    def read_files(self):
+        """
+        Lee un archivo Excel (.xlsx, .xls) o CSV (.csv) y devuelve un DataFrame.
+        """
+        try:
+            if self.input.endswith((".xlsx", ".xls")):
+                return pd.read_excel(self.input)
+            elif self.input.endswith(".csv"):
+                return pd.read_csv(self.input)
+            else:
+                raise ValueError("Formato no soportado. Usa .xlsx, .xls o .csv.")
+        except Exception as e:
+            print(f"Error al leer el archivo '{self.input}': {e}")
+            return None
 
 
-def split_column(df, source_col, new_cols, delimiter, max_splits=None):
-    """
-    Divide una columna en dos nuevas columnas eliminando la original.
-    """
-    if len(new_cols) < 2:
-        raise ValueError(
-            "Debes proporcionar al menos dos nombres para las nuevas columnas."
-        )
-
-    if source_col not in df.columns:
-        raise ValueError(f"La columna '{source_col}' no existe en el DataFrame.")
-
-    if max_splits is None:
-        max_splits = len(new_cols) - 1
-
-    # Dividir la columna y asignar a las nuevas columnas
-    df[new_cols] = (
-        df.pop(source_col)
-        .str.split(delimiter, n=max_splits, expand=True)
-        .apply(lambda x: x.str.strip())
-        .astype(str)
-    )
-
-    return df
-
-
-def numeric_columns(df, columns):
-    """
-    Limpia múltiples columnas numéricas eliminando comas y convierte a tipo numérico.
-    """
-    for col in columns:
-        if col in df.columns:
-            df[col] = pd.to_numeric(
-                df[col].astype(str).str.replace(",", ""), errors="coerce"
+    def split_column(self, source_col: str, new_cols: list[str], delimiter: str, max_splits=None):
+        """
+        Divide una columna en dos nuevas columnas eliminando la original.
+        """
+        if len(new_cols) < 2:
+            raise ValueError(
+                "Debes proporcionar al menos dos nombres para las nuevas columnas."
             )
-        else:
-            print(f"Advertencia: La columna '{col}' no existe en el DataFrame.")
-    return df
 
+        if source_col not in self.df.columns:
+            raise ValueError(f"La columna '{source_col}' no existe en el DataFrame.")
 
-def save_data(nombre_archivo, datos):
-    """
-    Guarda los datos extraídos en un archivo Excel.
-    """
-    try:
-        df = pd.DataFrame(datos)
-        df.to_excel(nombre_archivo, index=False)
-        print(f"Datos guardados correctamente en {nombre_archivo}")
-    except Exception as e:
-        print(f"Error al guardar en Excel: {e}")
+        if max_splits is None:
+            max_splits = len(new_cols) - 1
 
-
-def main():
-    """
-    Función principal para procesar los archivos extraídos.
-    - Lee los archivos extraídos.
-    - Divide columnas según la configuración.
-    - Convierte las últimas 8 columnas a numéricas.
-    - Guarda los datos procesados en un nuevo archivo.
-    """
-    for file_key, config_file in a_config.FILE_CONFIGS.items():
-        print(f"🔹 Procesando data: {file_key}")
-
-        archivo_entrada = os.path.join(
-            a_config.PATH_DATA_RAW, config_file.get("ARCHIVO_SCRAPING", "")
+        # Dividir la columna y asignar a las nuevas columnas
+        self.df[new_cols] = (
+            self.df.pop(source_col)
+            .str.split(delimiter, n=max_splits, expand=True)
+            .apply(lambda x: x.str.strip())
+            .astype(str)
         )
-        if not os.path.exists(archivo_entrada):
-            print(f"Archivo no encontrado: {archivo_entrada}. Se omite.")
-            continue
 
-        # Leer archivo
-        df = read_files(archivo_entrada)
+        return self.df
 
-        # Obtener configuración de limpieza
-        config_clean = a_config.CLEANING_CONFIGS.get(file_key, {})
-        encabezados = config_clean.get("ENCABEZADOS_PROCESADOS", [])
+    @staticmethod
+    def convert_to_numeric(df: pd.DataFrame):
+        """
+        Limpia múltiples columnas numéricas eliminando comas y convierte a tipo numérico.
+        """
+        df = df.apply(lambda col: pd.to_numeric(col.astype(str).str.replace(",", ""), errors="coerce"))
+        return df
+    
+    def normalize_dep_column(self):
+        for col in self.df.columns:
+            if col.lower() in "departamento" or "departamento" in col.lower():
+                self.df[col] = self.df[col].apply(lambda x: x.split(" ")[-1])
+                self.df[col] = self.df[col].apply(lambda x: ubg.validate_departamento(x, on_error="capitalize"))
+                self.df = self.df.rename(columns={col: "Departamento"})
+            elif col.lower() in "provincia" or "provincia" in col.lower():
+                self.df[col] = self.df[col].apply(lambda x: x.split(" ")[-1])
+                self.df[col] = self.df[col].apply(lambda x: ubg.validate_ubicacion(x, on_error="capitalize"))
+                self.df = self.df.rename(columns={col: "Provincia"})
+    
+
+    def save_data(self, nombre_archivo: str, datos):
+        """
+        Guarda los datos extraídos en un archivo Excel.
+        """
+        try:
+            df = pd.DataFrame(datos)
+            df.to_excel(nombre_archivo, index=False)
+            print(f"Datos guardados correctamente en {nombre_archivo}")
+        except Exception as e:
+            print(f"Error al guardar en Excel: {e}")
+
+
+    def main(self):
+        """
+        Función principal para procesar los archivos extraídos.
+        - Lee los archivos extraídos si es necesario.
+        - Convierte las últimas 8 columnas a numéricas.
+        - Guarda los datos procesados en un nuevo archivo.
+        """
+        
+        # for file_key, config_file in FILE_CONFIGS.items():
+        #     logging.info(f"🔹 Procesando data: {file_key}")
+
+        # Convertir las últimas 8 columnas a numéricas (si existen)
+        self.df.iloc[-8:] = self.convert_to_numeric(self.df, self.df.columns[-8:])
+        self.normalize_dep_column()
 
         # Identificar la primera columna original
         primera_columna = [df.columns[0]] if not df.empty else []
@@ -147,12 +168,11 @@ def main():
         # Reordenar: Primera columna original → columnas del split → otras columnas
         df = df[primera_columna + columnas_nuevas + columnas_restantes]
 
-        # Convertir las últimas 8 columnas a numéricas (si existen)
-        df = numeric_columns(df, df.columns[-8:])
+        
 
         # Guardar archivo procesado
         archivo_salida = os.path.join(
-            a_config.PATH_DATA_PRO,
+            PATH_DATA_PRO,
             f"PROCESADO_{config_file.get('ARCHIVO_SCRAPING', '')}",
         )
         save_data(archivo_salida, df)
